301b241f59092a97dc1c6f6e3d2d275371236692
Ankit Kumar Singh <77728943+SinghAnkIT1812@users.noreply.github.com>
2021-01-22 17:28:21 +0530

    Add files via upload

---
+++ irctc.py
@@ -0,0 +1,46 @@
+import requests
+class IRCTC:
+
+    def __init__(self):
+        user_input=input("""how would you like to procde?
+        1. enter 1 to check live train status
+        2.enter 2 to check pnr
+        3.enter 3 to check train schedule""")
+
+        if user_input=="1":
+           self.live_status()
+        elif user_input =="2":
+            print("pnr")
+        else:
+            self.train_schedule()
+    def live_status(self):
+        train_no=input("enter the train no")
+        year=input("enter the year")
+        month=input("enter the month")
+        day=input("enter the day")
+        hello=year+month+day
+        self.fetch_traindata(train_no,hello)
+
+    def fetch_traindata(self,train_no,hello):
+        data=requests.get("https://indianrailapi.com/api/v2/livetrainstatus/apikey/9485fe667278d32f90d3337b8e025db0/trainnumber/{}/date/{}".format(train_no,hello))    
+        data=data.json()
+        print(data['TrainRoute'])
+
+        for i in data['TrainRoute']:
+            print(i['StationName'],"|",i["ScheduleArrival"],"|",i["ActualArrival"],"|",i["ActualDeparture"],"pm")
+    
+    
+    def train_schedule(self):
+        train_no=input("enter the train no")
+        self.fetch_data(train_no)
+
+    def fetch_data(self,train_no):
+        data=requests.get("https://indianrailapi.com/api/v2/TrainSchedule/apikey/9485fe667278d32f90d3337b8e025db0/TrainNumber/{}".format(train_no))
+        data = data.json()
+
+        print(data['Route'])
+
+        for i in data['Route']:
+            print(i['StationName'],"|",i["ArrivalTime"],"|",i["DepartureTime"],"|",i["Distance"],"kms")
+
+obj = IRCTC()
